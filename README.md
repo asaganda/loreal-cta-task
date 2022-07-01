391f0283f4b9baedb765dcbbc08c7913ae2c7b50
Andrew <Asaganda@gmail.com>
2022-07-01 16:53:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# loreal-cta-task
+This task was for a job at L'Oreal. The task is to change the CTA position right below the product title and change the CTA color all with vanilla javascript
---
+++ loreal-cta.js
@@ -0,0 +1,5 @@
+const nameWrapper = document.querySelector('.c-product-main__name-wrapper')
+const ctaWrapper = document.querySelector('.c-product-main__actions.m-actions')
+
+nameWrapper.after(ctaWrapper);
+document.querySelector('.c-product-add-bag').style.backgroundColor = '#2ab599';
